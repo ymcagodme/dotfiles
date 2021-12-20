64329dfd3b108a712f35c70a50c51b84503df714
David Huang <huangdavid@google.com>
2021-12-20 12:28:18 +0800

    Add a space padding when display vimrc finder

--- nvim/.config/nvim/lua/pid/telescope.lua
+++ nvim/.config/nvim/lua/pid/telescope.lua
@@ -15,11 +15,12 @@ require("telescope").setup({
     },
     defaults = {
         file_sorter = require("telescope.sorters").get_fzy_sorter,
-        prompt_prefix = " >",
+        prompt_prefix = " > ",
         color_devicons = true,
 
         -- winblend: Transparency
         winblend = 0,
+        file_ignore_patterns = {"venv/", "__pycache__/", "*.pyc"},
 
         file_previewer = require("telescope.previewers").vim_buffer_cat.new,
         grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
@@ -38,7 +39,7 @@ require("telescope").load_extension("fzy_native")
 local M = {}
 M.search_dotfiles = function()
     require("telescope.builtin").find_files({
-        prompt_title = "< VimRC >",
+        prompt_title = "< VimRC > ",
         cwd = '~/.config/nvim',
         file_ignore_patterns = {"autoload/", "undodir"},
         hidden = true,
