3dfde92cf43db6ac6cee3b92341e86558edf3ed8
David Huang <huangdavid@google.com>
2022-01-21 16:56:51 +0800

    [nvim] Fix: transparency in nvim

--- nvim/.config/nvim/lua/user/options.lua
+++ nvim/.config/nvim/lua/user/options.lua
@@ -3,40 +3,39 @@
 
 local options = {
 
-  -- General settings
-  mouse = "a",
-  number = true,
-  relativenumber = true,
-  hlsearch = false,
-  errorbells = false,
-  wrap = false,
-  colorcolumn = "80",
-  -- Read vimrc recursively (in folders)
-  exrc = true,
-  -- Don't unload a buffer when no longer shown in a window
-  hidden = true,
-  -- Keep 8 lines margin
-  scrolloff = 8,
-  -- Have a sign column on the left to indicate status
-  signcolumn = "yes",
-  -- faster completion (4000ms default)
-  updatetime = 300,
+	-- General settings
+	mouse = "a",
+	number = true,
+	relativenumber = true,
+	hlsearch = false,
+	errorbells = false,
+	wrap = false,
+	colorcolumn = "80",
+	-- Read vimrc recursively (in folders)
+	exrc = true,
+	-- Don't unload a buffer when no longer shown in a window
+	hidden = true,
+	-- Keep 8 lines margin
+	scrolloff = 8,
+	-- Have a sign column on the left to indicate status
+	signcolumn = "yes",
+	-- faster completion (4000ms default)
+	updatetime = 300,
 
-  
-  -- Tab settings
-  tabstop = 4,
-  softtabstop = 4,
-  shiftwidth = 4,
-  expandtab = true,
-  smartindent = true,
-  
-  -- History settings
-  swapfile = false,
-  backup = false,
-  undofile = true,
+	-- Tab settings
+	tabstop = 4,
+	softtabstop = 4,
+	shiftwidth = 4,
+	expandtab = true,
+	smartindent = true,
+
+	-- History settings
+	swapfile = false,
+	backup = false,
+	undofile = true,
 }
 
 -- Load all options.
 for k, v in pairs(options) do
-  vim.opt[k] = v
+	vim.opt[k] = v
 end
